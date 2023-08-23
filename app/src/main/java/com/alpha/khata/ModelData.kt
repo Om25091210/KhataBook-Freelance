112852a3351e4b5a03160a17db749f799b3f8978
Omkumar <74484315+Om25091210@users.noreply.github.com>
2023-08-23 23:06:33 +0530

    Initial project set-up

---
+++ app/src/main/java/com/alpha/khata/ModelData.kt
@@ -0,0 +1,14 @@
+package com.alpha.khata
+
+data class ModelData(
+    val id: String,
+    val amount: String,
+    val data: String,
+    val date: String,
+    val statuses: String,
+    val cus_nama: String,
+    val customer_num: String,
+    val paym: String,
+    val dattime: String,
+)
+
